df599a681df9ca18fdfef49145947df9240f92e3
deblur99 <deblur99@kakao.com>
2026-06-15 10:40:18 +0900

    Initial Commit

---
+++ InterviewTutor/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  InterviewTutor
+//
+//  Created by 한현민 on 6/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ InterviewTutorTests/InterviewTutorTests.swift
@@ -0,0 +1,19 @@
+//
+//  InterviewTutorTests.swift
+//  InterviewTutorTests
+//
+//  Created by 한현민 on 6/15/26.
+//
+
+import Testing
+@testable import InterviewTutor
+
+struct InterviewTutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
